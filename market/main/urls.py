e8b98bffaf445a9712f338e4ab7caf54d76926e0
Federico <federicolucaspontoriero@gmail.com>
2022-12-09 01:09:32 -0300

    django project

---
+++ market/main/urls.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from django.urls import path
+from main import views
+
+urlpatterns = [
+    path("home/", views.homepage, name="home"),
+    path("items/", views.itemspage, name="items"),
+]
---
+++ market/main/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def homepage(request):
+    return render(request, template_name="main/home.html")
+
+
+def itemspage(request):
+    items = [{"name": "Phone", "price": "500"}, {"name": "Laptop", "price": "1000"}]
+    return render(request, template_name="main/items.html", context={"items": items})
