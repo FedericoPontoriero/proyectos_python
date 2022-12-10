9df72d142d04d4476080e8c2eeb17c409e5269e0
Federico <federicolucaspontoriero@gmail.com>
2022-12-10 17:37:05 -0300

    finished practice project with Django

--- market/main/urls.py
+++ market/main/urls.py
@@ -5,4 +5,6 @@ from main import views
 urlpatterns = [
     path("home/", views.homepage, name="home"),
     path("items/", views.itemspage, name="items"),
+    path("login/", views.loginpage, name="login"),
+    path("register/", views.registerpage, name="register"),
 ]
